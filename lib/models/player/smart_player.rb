3455a74cd34951fb0fc294c02e7631de2f481553
Raphael Quintanilha dos Santos <raphael_santos@id.uff.br>
2017-11-13 19:27:59 -0200

    change on negamax method

--- lib/models/player/smart_player.rb
+++ lib/models/player/smart_player.rb
@@ -10,16 +10,18 @@ module Model
         Action.new(performer: self, space: selected_space)
       end
 
+      private
+
       def negamax(board:, depth: 0, alpha: -1000, beta: 1000, color: 1, another_player:)
         return 0 if board.tied?
         return color * (1000 / depth) if board.conquered_by?(player: self)
         return color * (-1000 / depth) if board.conquered_by?(player: another_player)
         max = -1000
+        player = color.positive? ? self : another_player
         board.available_spaces.each do |space|
-          player = color.positive? ? self : another_player
           action = Action.new(performer: player, space: space)
           action.apply
-          negamax_value = -negamax(board: board, depth: depth+1, beta: -beta, alpha: -alpha, color: -color, another_player: another_player)
+          negamax_value = -negamax(board: board, depth: depth+1, alpha: -beta, beta: -alpha, color: -color, another_player: another_player)
           action.undo
           max = [max, negamax_value].max
           @best_score[space] = max if depth == 0
